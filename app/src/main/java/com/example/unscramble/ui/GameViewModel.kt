47c60aa593c75bc697512378e18d07e983787922
RyoTanabe <rtanabe@asx.co.jp>
2024-12-31 20:23:01 +0900

    Design the Compose UI.

--- app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
+++ app/src/main/java/com/example/unscramble/ui/GameViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.unscramble.ui
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.unscramble.data.allWords
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -11,6 +14,9 @@ class GameViewModel: ViewModel() {
     val uiState: StateFlow<GameUiState> = _uiState.asStateFlow()
     private lateinit var currentWord: String
     private var usedWords: MutableSet<String> = mutableSetOf() //ゲームで使用済みの単語を格納する
+    var userGuess by mutableStateOf("")
+        private set
+
 
     private fun  pickRandomWordAndShuffle(): String {
         currentWord = allWords.random()
@@ -29,6 +35,10 @@ class GameViewModel: ViewModel() {
             tempWord.shuffle()
         }
         return String(tempWord)
+    }
+
+    fun updateUserGuess(guessedWord: String){
+        userGuess = guessedWord
     }
 
     fun resetGame(){
